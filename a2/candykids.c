a37d8f25b140adba0d9357269a176ed5b02a6846
rstolys <44356584+rstolys@users.noreply.github.com>
2021-02-25 14:17:36 -0800

    New Assignment
    Added .c and .h files for assignment 2.
    Generated makefile.
    Included file framework for each file.
    .h files do not have declarations

---
+++ a2/candykids.c
@@ -0,0 +1,45 @@
+/*-----------------------------------------------------------------------------
+ * candykids.c -- Main program for assignment 2
+ *
+ * 25 Feb 2021	Ryan Stolys, Jayden Cole
+ *
+ -----------------------------------------------------------------------------*/
+
+/***INCLUDES******************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+
+/***DEFINES*******************************************************************/
+#define ERROR              -1
+#define SUCCESS             0
+#define FAILURE             1
+
+//For debugging 
+//#define DEBUG
+
+/***GLOBAL VARIABLES**********************************************************/
+
+
+/***FUNCTION DECLARATIONS*****************************************************/
+
+
+
+/***FUNCTION DEFINITIONS******************************************************/
+
+
+
+/*******************************************************************
+** MAIN FUNCTION -- test program
+**
+** @param[in]  argc    number of command line arguements
+** @param[in]  argv    array of command line arguements
+**
+********************************************************************/
+int main(int argc, char **argv)
+    {
+
+    return SUCCESS;
+    }
