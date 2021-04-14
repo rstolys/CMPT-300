d69ff99d3b662c6a3024e42322f11e5278580783
rstolys <44356584+rstolys@users.noreply.github.com>
2021-04-13 19:03:48 -0700

    Added user space emulation

--- a4/array_stats_usr.c
+++ a4/array_stats_usr.c
@@ -7,11 +7,10 @@
 
 /***INCLUDES******************************************************************/
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
 
-#include "array_stats.h"
+#include "array_stats_usr.h"
 
 
 /***DEFINES*******************************************************************/
@@ -27,7 +26,6 @@
 
 /***FUNCTION DEFINITIONS******************************************************/
 
-
 /*******************************************************************
 ** sys_array_stats -- compute array_stats in kernel -- syscall 437
 **
@@ -37,7 +35,7 @@
 ** @param[out]  stats       the output array_stats
 **
 ********************************************************************/
-asmlinkage long sys_array_stats(struct array_stats *stats, long *data, long size)
+long sys_array_stats(struct array_stats *stats, long *data, long size)
     {
     //Define local variables
     int                     rv = SUCCESS;
@@ -45,24 +43,27 @@ asmlinkage long sys_array_stats(struct array_stats *stats, long *data, long size
     struct array_stats      arrStats = {0};
 
     //Print inputs
-    printk(KERN_INFO "INPUTS OF SYS_ARRAY_STATS: *stats: %p, *data: %p, size: %ld\n", stats, data, size); 
+    printf("INPUTS OF SYS_ARRAY_STATS: *stats: %p, *data: %p, size: %ld\n", stats, data, size); 
+
+    memcpy(&tempMem, data, sizeof(long));
+    memcpy(&arrStats, stats, sizeof(struct array_stats));
 
     //Check value size of array 
     if(size <= 0)
         {
-        printk(KERN_ERR "Invalid input: size\n"); 
+        printf("Invalid input: size\n"); 
         rv = -EINVAL;
         }
     // Input data is NULL or invalid for user
-    else if(data == NULL || (0 != copy_from_user(&tempMem, data, sizeof(long))))
+    else if(data == NULL)
         {
-        printk(KERN_ERR "Invalid input: data\n"); 
+        printf("Invalid input: data\n"); 
         rv = -EFAULT;
         }
     // Output pointer is null or invalid for user
-    else if(stats == NULL || (0 != copy_from_user(&arrStats, stats, sizeof(struct array_stats))))
+    else if(stats == NULL)
         {
-        printk(KERN_ERR "Invalid input: stats\n"); 
+        printf("Invalid input: stats\n"); 
         rv = -EFAULT;
         }
     else 
@@ -76,34 +77,44 @@ asmlinkage long sys_array_stats(struct array_stats *stats, long *data, long size
 
         for(long i = 1; i < size; i++)
             {
-            if(0 != copy_from_user(&tempMem, data + i, sizeof(long)))
-                {
-                printk(KERN_ERR "Invalid data element in array. Element #%ld\n", i); 
-                rv = -EFAULT;
-                break;
-                }
-            else 
-                {
-                 //Check and set min value
-                if(arrStats.min > tempMem)
-                    arrStats.min = tempMem;
+            memcpy(&tempMem, data + i, sizeof(long));
+            
+            //Check and set min value
+            if(arrStats.min > tempMem)
+                arrStats.min = tempMem;
 
-                //Check and set max value
-                if(arrStats.max < tempMem)
-                    arrStats.max = tempMem;
+            //Check and set max value
+            if(arrStats.max < tempMem)
+                arrStats.max = tempMem;
 
-                //Increase the sum of the array values
-                arrStats.sum += tempMem;
-                }
+            //Increase the sum of the array values
+            arrStats.sum += tempMem;
             }
 
         //Set the array stats output if we are here without here (rv is still == SUCCESS)
-        if(rv == SUCCESS && 0 != copy_to_user(stats, &arrStats, sizeof(struct array_stats)))
-            {
-            printk(KERN_ERR "Invalid copy of array_stats to user\n"); 
-            rv = -EFAULT;
-            }
+        memcpy(stats, &arrStats, sizeof(struct array_stats));
         }
 
     return rv;
+    }
+
+
+/*******************************************************************
+** MAIN FUNCTION -- test program
+**
+** @param[in]  argc    number of command line arguements
+** @param[in]  argv    array of command line arguements
+**
+********************************************************************/
+int main(int argc, char* argv[]) 
+    {
+    struct array_stats testStat = {0}; 
+    long dataIn[1] = {1};
+
+    int result = sys_array_stats(&testStat, dataIn, 1);
+
+    printf("RESULT IS: %d\n", result);
+    printf("STATS ARE: min: %ld, max: %ld, sum: %ld\n", testStat.min, testStat.max, testStat.sum);
+
+    return 0;
     }
