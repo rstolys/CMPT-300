cbe89cd9962ed99555cf3f304bb7536761c184f0
Rep7rax2 <rstolys>
2021-04-18 18:41:49 -0700

    added array stats code

---
+++ a4/test/array_stats.h
@@ -0,0 +1,28 @@
+/*-----------------------------------------------------------------------------
+ * array_stats.h -- Array Stats Sys Call Header File
+ *
+ * 19 Apr 2021	Ryan Stolys, Jayden Cole
+ *
+ -----------------------------------------------------------------------------*/
+
+#ifndef _ARRAY_STATS_H_
+#define _ARRAY_STATS_H_
+
+/***INCLUDES******************************************************************/
+//NONE
+
+/***DEFINES*******************************************************************/
+//None
+
+
+/***GLOBAL VARIABLES**********************************************************/
+struct array_stats {
+  long min;
+  long max;
+  long sum;
+};
+
+
+//asmlinkage long sys_array_stats(struct array_stats *stats, long *data, long size);
+
+#endif /*_ARRAY_STATS_H_*/
